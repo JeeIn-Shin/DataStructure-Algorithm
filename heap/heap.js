495d48427b22f96351032da3608f7b406054cca1
JeeIn-Shin <wldls0530@gmail.com>
2023-01-05 20:13:33 +0900

    heap implementation completed

--- heap/heap.js
+++ heap/heap.js
@@ -20,17 +20,17 @@
 //MAX Heap을 기준으로 함
 class Heap {
 
-    constructor()   {
+    constructor() {
         //배열의 첫 원소는 사용하지 X
-        this.heap = [];
+        this.heap = [ null ];
     }
 
-    heapsize()  {
+    heapsize() {
         return this.heap.length;
     }
 
     //heap 삽입
-    insert(data)  {
+    insert(data) {
         //트리(배열)의 마지막에 삽입함
         //그러니까, 배열의 크기를 알아야하는데
         //이건 heapsize()함수를 이용할거임
@@ -39,7 +39,7 @@ class Heap {
 
         //부모 노드와 삽입된 data 값 비교를 해야하는데
         //그러면 부모의 위치를 알아야함
-        let parent = Math.floor((index - 1) / 2)
+        let parent = (index - 1) / 2;
 
         //삽입된 data가 더 큰 숫자라면 교환
         // if(parent < index)   {
@@ -54,20 +54,71 @@ class Heap {
         //while문이 계속 헛돌길래 뭔지 몰랐는데
         // this.heap[parent] < thie.heap[data]
         //이렇게 비교해주고있었음
-        while(this.heap[parent] < this.heap[index])    {
+
+        if(this.heapsize() == 2)
+            return ;
+
+        while (this.heap[parent] < this.heap[index]) {
             [this.heap[parent], this.heap[index]] = [this.heap[index], this.heap[parent]]
             index = parent;
-            parent = Math.floor((index - 1) / 2);
+            parent = Math.floor((index) / 2);
+
+            if(parent == 0)
+                return ;
         }
     }
 
     //heap 삭제
-    heappop()   {
-        //루트노드가 삭제됨
+    delete() {
 
         //삭제된 루트 노드에 힙의 마지막 노드를 가져옴
+        //힙의 마지막 노드를 알려면? 
+        //이 lastNode를 어떻게 위에다가 넣지?
+        let parent = 1;
+        let child = 2;
+        let last = this.heapsize() - 1;
+        let lastValue = this.heap[last];
+
+        [this.heap[last], this.heap[1]] = [this.heap[1], this.heap[last]]
+        let returnValue = this.heap.pop();
 
         //힙 재구성 --> 어떻게??
+        //루트 노드와 자식 노드의 위치를 교환해주는데,
+        //이때 자식 노드들 중 더 큰 값을 가진 자식 노드와 교환해줘야한다.
+        // if(this.heap[child] < this.heap[child + 1])
+        //     [this.heap[parent], this.heap[child + 1]] = [this.heap[child + 1], this.heap[parent]]
+        // else
+        //     [this.heap[parent], this.heap[child]] = [this.heap[child], this.heap[parent]]
+
+        //근데 트리 레벨을 내려가면서 정리해줘야하는거니까
+        //언제까지 돌아야하지? 
+        while (child <= this.heap.length) {
+
+
+            //아 이거 꼬이는데;
+
+            if (this.heap[child] < this.heap[child + 1])
+                [this.heap[parent], this.heap[child + 1]] = [this.heap[child + 1], this.heap[parent]]
+            else
+                [this.heap[parent], this.heap[child]] = [this.heap[child], this.heap[parent]]
+
+            //세상에 여기가 문제였음;
+            //아니 그러니까.. 음
+            //처음에 이 부분을 this.heap[last] 라고 써놔서
+            //계속 헛돌면서 종료가 안됐음....
+            //근데 현재 시점에서 heap의 가장 마지막 값이 아니라, 삭제되기 전 heap의 마지막 값을 봐야했음.. 
+            if(lastValue >= this.heap[child])
+                return ;
+
+            //레벨 아래로 이동
+            parent = child;
+            child *= 2;
+        }
+
+        this.heap[parent] = last;
+
+
+        return returnValue;
     }
 
 }
@@ -80,5 +131,8 @@ heap.insert(4);
 heap.insert(0);
 heap.insert(7);
 
+console.log(heap);
+
+heap.delete();
 
 console.log(heap);
---
+++ heap/test.js
@@ -0,0 +1,72 @@
+//https://velog.io/@longroadhome/%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0-JS%EB%A1%9C-%EA%B5%AC%ED%98%84%ED%95%98%EB%8A%94-HEAP
+//비교를 위해 남의 코드 긁어옴
+
+class MinHeap {
+    constructor() {
+        this.heap = [ null ];
+    }
+    
+    size() {
+        return this.heap.length - 1;
+    }
+    
+    getMin() {
+        return this.heap[1] ? this.heap[1] : null;
+    }
+    
+    swap(a, b) {
+        [ this.heap[a], this.heap[b] ] = [ this.heap[b], this.heap[a] ];
+    }
+    
+    heappush(value) {
+        this.heap.push(value);
+        let curIdx = this.heap.length - 1;
+        let parIdx = (curIdx / 2) >> 0;
+        
+        while(curIdx > 1 && this.heap[parIdx] > this.heap[curIdx]) {
+            this.swap(parIdx, curIdx)
+            curIdx = parIdx;
+            parIdx = (curIdx / 2) >> 0;
+        }
+    }
+    
+    heappop() {
+        const min = this.heap[1];	
+        if(this.heap.length <= 2) this.heap = [ null ];
+        else this.heap[1] = this.heap.pop();   
+        
+        let curIdx = 1;
+        let leftIdx = curIdx * 2;
+        let rightIdx = curIdx * 2 + 1; 
+        
+        if(!this.heap[leftIdx]) return min;
+        if(!this.heap[rightIdx]) {
+            if(this.heap[leftIdx] < this.heap[curIdx]) {
+                this.swap(leftIdx, curIdx);
+            }
+            return min;
+        }
+
+        while(this.heap[leftIdx] < this.heap[curIdx] || this.heap[rightIdx] < this.heap[curIdx]) {
+            const minIdx = this.heap[leftIdx] > this.heap[rightIdx] ? rightIdx : leftIdx;
+            this.swap(minIdx, curIdx);
+            curIdx = minIdx;
+            leftIdx = curIdx * 2;
+            rightIdx = curIdx * 2 + 1;
+        }
+
+        return min;
+    }
+}
+
+
+let minheap = new MinHeap();
+
+minheap.heappush(5);
+minheap.heappush(1);
+minheap.heappush(4);
+minheap.heappush(0);
+minheap.heappush(7);
+
+
+console.log(minheap);
